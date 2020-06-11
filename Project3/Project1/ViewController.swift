fb9066e75a11e1cd0ce9bd416d9b87227360ad89
soviet47 <promachov@gmail.com>
2020-06-10 21:52:16 -0300

    Adding next projects!

---
+++ Project3/Project1/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Peter Romachov on 6/6/20.
+//  Copyright © 2020 Peter Romachov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                // this is a picture to load!
+                pictures.append(item)
+            }
+        }
+        pictures.sort()
+        print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = "Picture \(indexPath.row + 1) out of \(pictures.count)"
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController{
+            vc.selectedImage = pictures[indexPath.row]
+            vc.imageIndex = indexPath.row
+            vc.totalImages = pictures.count
+            navigationController?.pushViewController(vc, animated: true)
+        }
+         
+    }
+    
+    @objc func shareTapped() {
+        let vc = UIActivityViewController(activityItems: ["This app is amazing!"], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+
+}
+
